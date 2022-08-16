7f4c1bd32b8508a0a8243fd176d1f3d035270f5d
bloomstick <twistappscontact@gmail.com>
2022-08-16 21:20:13 +0300

    feat: Fetch and Post requests

---
+++ Scripts/Requests/Integrations/Modula/FetchModule.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Modula;
+using Modula.Common;
+using Modula.Optimization;
+
+namespace RequestForMirror.Integrations.Modula
+{
+    public abstract class FetchModule<TResponse> : Fetch<TResponse>, IModule
+    {
+        private ModuleDefaultImplementation _defaultImplementation;
+        // ReSharper disable once MemberCanBePrivate.Global
+        protected ModuleDefaultImplementation DefaultImplementation
+        {
+            get { return _defaultImplementation ??= new ModuleDefaultImplementation(this); }
+        }
+
+        public virtual void Update()
+        {
+            DefaultImplementation.Update();
+        }
+
+        public TimingConstraints UpdateInvocationConstraints => DefaultImplementation.UpdateConstraints;
+        public virtual TypedList<IModule> RequiredOtherModules { get; } = new TypedList<IModule>();
+
+        public ModularBehaviour Main => DefaultImplementation.Main;
+
+        public void OnAdd()
+        {
+            DefaultImplementation.OnAdd();
+        }
+
+        public void AddModule(Type moduleType)
+        {
+            DefaultImplementation.AddModule(moduleType);
+        }
+
+        public string GetName()
+        {
+            return DefaultImplementation.GetName();
+        }
+
+        public DataLayer GetData()
+        {
+            return DefaultImplementation.GetData();
+        }
+
+        public void ManagedUpdate()
+        {
+            ModuleUpdate();
+        }
+
+        public virtual bool ShouldSerialize()
+        {
+            return true;
+        }
+
+        protected virtual void ModuleUpdate()
+        {
+        }
+    }
+}
---
+++ Scripts/Requests/Integrations/Modula/PostModule.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Modula;
+using Modula.Common;
+using Modula.Optimization;
+
+namespace RequestForMirror.Integrations.Modula
+{
+    public abstract class PostModule<TRequest, TResponse> : Post<TRequest, TResponse>, IModule
+    {
+        private ModuleDefaultImplementation _defaultImplementation;
+        // ReSharper disable once MemberCanBePrivate.Global
+        protected ModuleDefaultImplementation DefaultImplementation
+        {
+            get { return _defaultImplementation ??= new ModuleDefaultImplementation(this); }
+        }
+
+        public virtual void Update()
+        {
+            DefaultImplementation.Update();
+        }
+
+        public TimingConstraints UpdateInvocationConstraints => DefaultImplementation.UpdateConstraints;
+        public virtual TypedList<IModule> RequiredOtherModules { get; } = new TypedList<IModule>();
+
+        public ModularBehaviour Main => DefaultImplementation.Main;
+
+        public void OnAdd()
+        {
+            DefaultImplementation.OnAdd();
+        }
+
+        public void AddModule(Type moduleType)
+        {
+            DefaultImplementation.AddModule(moduleType);
+        }
+
+        public string GetName()
+        {
+            return DefaultImplementation.GetName();
+        }
+
+        public DataLayer GetData()
+        {
+            return DefaultImplementation.GetData();
+        }
+
+        public void ManagedUpdate()
+        {
+            ModuleUpdate();
+        }
+
+        public virtual bool ShouldSerialize()
+        {
+            return true;
+        }
+
+        protected virtual void ModuleUpdate()
+        {
+        }
+    }
+
+    public abstract class PostModule<TRequest, TRequest2, TResponse> : Post<TRequest, TRequest2, TResponse>, IModule
+    {
+        private ModuleDefaultImplementation _defaultImplementation;
+        // ReSharper disable once MemberCanBePrivate.Global
+        protected ModuleDefaultImplementation DefaultImplementation
+        {
+            get { return _defaultImplementation ??= new ModuleDefaultImplementation(this); }
+        }
+
+        public virtual void Update()
+        {
+            DefaultImplementation.Update();
+        }
+
+        public TimingConstraints UpdateInvocationConstraints => DefaultImplementation.UpdateConstraints;
+        public virtual TypedList<IModule> RequiredOtherModules { get; } = new TypedList<IModule>();
+
+        public ModularBehaviour Main => DefaultImplementation.Main;
+
+        public void OnAdd()
+        {
+            DefaultImplementation.OnAdd();
+        }
+
+        public void AddModule(Type moduleType)
+        {
+            DefaultImplementation.AddModule(moduleType);
+        }
+
+        public string GetName()
+        {
+            return DefaultImplementation.GetName();
+        }
+
+        public DataLayer GetData()
+        {
+            return DefaultImplementation.GetData();
+        }
+
+        public void ManagedUpdate()
+        {
+            ModuleUpdate();
+        }
+
+        public virtual bool ShouldSerialize()
+        {
+            return true;
+        }
+
+        protected virtual void ModuleUpdate()
+        {
+        }
+    }
+}
