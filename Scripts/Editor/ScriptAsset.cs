92d375400e0b2cac26f3a89983af7d4fcf1b4f7a
bloomstick <twistappscontact@gmail.com>
2022-08-18 19:17:11 +0300

    feat: delete generated scripts if original is deleted

---
+++ Scripts/Editor/ScriptAsset.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace RequestForMirror.Editor.CodeGen
+{
+    public class ScriptAsset
+    {
+        private readonly string _path; //without extension
+
+        private void WarnIfUnintendedExtension(string path)
+        {
+            var extension = Path.GetExtension(path);
+            if (extension != string.Empty 
+                && extension != ".meta" 
+                && extension != ".cs")
+            {
+                Debug.LogWarning($"ScriptAsset is not intended to use with {extension}. Use it with .cs or .meta files only");
+            }
+        }
+        
+        public ScriptAsset(params string[] path)
+        {
+            var concatenatedPath = Path.Combine(path);
+            WarnIfUnintendedExtension(concatenatedPath);
+            _path = Path.ChangeExtension(concatenatedPath, "");
+        }
+
+        public string CsPath => Path.ChangeExtension(_path, ".cs");
+        public string MetaPath => CsPath + ".meta";
+        public string Classname => Path.GetFileNameWithoutExtension(_path);
+
+        public bool Delete()
+        {
+            var fileDidExist = false;
+            foreach (var file in new[] {CsPath, MetaPath})
+            {
+                if (!File.Exists(file)) continue;
+                
+                File.Delete(file);
+                fileDidExist = true;
+            }
+
+            return fileDidExist;
+        }
+    }
+}
