30d53aaa4986777d6e7f1734cc731b32f0e87426
bloomstick <twistappscontact@gmail.com>
2022-08-19 14:35:04 +0300

    feat: Ok and Error properties: resolve #8

--- Scripts/IMarkedForCodeGen.cs
+++ Scripts/IMarkedForCodeGen.cs
@@ -1,6 +1,10 @@
-﻿/// <summary>
+﻿using RequestForMirror;
+
+/// <summary>
 ///     Used as a mark for code generator.
 /// </summary>
 public interface IMarkedForCodeGen
 {
+    RequestStatus Ok { get; }
+    RequestStatus Error { get; }
 }
