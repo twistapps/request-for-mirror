a427b645be994173295a803cb79e4638ec2b6d63
bloomstick <twistappscontact@gmail.com>
2022-08-20 21:59:01 +0300

    feat: add define symbols on first run

---
+++ Scripts/Editor/Setup/DefineSymbolsSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace RequestForMirror.Editor.Setup
+{
+    public static class DefineSymbolsSetter
+    {
+        [InitializeOnLoadMethod]
+        public static void AddDefineSymbols()
+        {
+            var buildTarget = EditorUserBuildSettings.selectedBuildTargetGroup;
+            
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTarget);
+            var symbolsHashSet = new HashSet<string>(symbols.Split(';')) { "REQUEST_FOR_MIRROR" };
+
+            var modifiedSymbols = string.Join(";", symbolsHashSet);
+            if (symbols == modifiedSymbols) return;
+            
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTarget, modifiedSymbols);
+            Debug.Log("Adding 'REQUEST_FOR_MIRROR' to scripting defines...");
+        }
+    }
+}
