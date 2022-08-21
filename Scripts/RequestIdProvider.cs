5d1232ce19bbdcda7c7c8c0be669a322edaefca5
bloomstick <twistappscontact@gmail.com>
2022-08-21 23:23:47 +0300

    feat: id provider for requests (#12)

---
+++ Scripts/RequestIdProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Mirror;
+
+namespace RequestForMirror
+{
+    public class RequestId
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public int ID;
+
+        private RequestId(int id)
+        {
+            ID = id;
+        }
+
+        public RequestId Next()
+        {
+            ID++;
+            return new RequestId(ID);
+        }
+        
+        public static implicit operator RequestId(int n) => new RequestId(n);
+        public static explicit operator int(RequestId request) => request.ID;
+    }
+    
+    public static class RequestIdProvider
+    {
+        private static readonly Dictionary<int, RequestId> RequestIdsPerClient = new Dictionary<int, RequestId>();
+
+        public static RequestId LocalRequestId = 0;
+
+        static RequestIdProvider()
+        {
+            NetworkServer.OnConnectedEvent += OnConnectedEvent;
+            NetworkServer.OnDisconnectedEvent += OnDisconnectedEvent;
+        }
+
+        private static void OnConnectedEvent(NetworkConnectionToClient conn)
+        {
+            RequestIdsPerClient.Add(conn.connectionId, 0);
+        }
+        
+        private static void OnDisconnectedEvent(NetworkConnectionToClient conn)
+        {
+            RequestIdsPerClient.Remove(conn.connectionId);
+        }
+
+        public static RequestId GenerateId(NetworkConnection conn)
+        {
+            var id = RequestIdsPerClient[conn.connectionId].Next();
+            return id;
+        }
+    }
+}
