89e22b9c073eab5d4de26d34d58ab554c6d374a4
bloomstick <twistappscontact@gmail.com>
2022-08-21 19:39:20 +0300

    feat: set builder variables based on serialization

--- Scripts/Editor/Request/SerializationSettingsPreprocessor.cs
+++ Scripts/Editor/Request/SerializationSettingsPreprocessor.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using RequestForMirror.Editor.CodeGen;
 using RequestForMirror.Utils;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace RequestForMirror.Editor.Request
 {
+    [SuppressMessage("ReSharper", "ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator")]
     public static class SerializationSettingsPreprocessor
     {
         [DidReloadScripts]
@@ -13,18 +17,57 @@ namespace RequestForMirror.Editor.Request
             CodeGen.CodeGen.OnBeforeCsFileGeneration += OnBeforeCsFileGeneration;
         }
 
+        private static void SetGenericArgsToString(CodeGenTemplateBuilder builder)
+        {
+            var variables = new Dictionary<string, string>(builder.Variables);
+            foreach (var variable in variables)
+            {
+                Debug.Log($"{variable.Key} -- {variable.Key.Contains(CodeGenTemplateBuilder.GenericArgumentSlug)}");
+                if (variable.Key.Contains(CodeGenTemplateBuilder.GenericArgumentSlug))
+                {
+                    builder.SetVariable(variable.Key, "string");
+                    Debug.Log($"Set {variable.Key} to 'string'");
+                }
+            }
+        }
+
+        private static void SetResponseVariable(CodeGenTemplateBuilder builder, Type type)
+        {
+            var variables = builder.Variables;
+            var genericArguments = type.BaseType!.GetGenericArguments();
+            var argumentCount = genericArguments.Length;
+        
+            var responseIndex = argumentCount > 1 ? $"_{argumentCount}" : "";
+            var key = $"{CodeGenTemplateBuilder.BaseSlug}_{CodeGenTemplateBuilder.GenericArgumentSlug}";
+            
+            builder.SetVariable("RESPONSE_TYPE", variables[key + responseIndex]);
+        }
+        
+        [SuppressMessage("ReSharper", "InvertIf")]
+        [SuppressMessage("ReSharper", "ConvertIfStatementToSwitchStatement")]
+        [SuppressMessage("ReSharper", "RedundantJumpStatement")]
         private static void OnBeforeCsFileGeneration(CodeGenTemplateBuilder builder, Type type)
         {
+            Debug.Log("Preprocessing codegen file...");
+            if (!typeof(IRequest).IsAssignableFrom(type)) return;
+            Debug.Log("Assignability test passed");
+            
+            
             var settings = SettingsUtility.Load<RequestSettings>();
-            if (settings.serializationMethod != RequestSerializerType.JsonUtility) return;
+            var serializerInUse = settings.serializationMethod;
 
-            for (var i = 0; i < 4; i++)
+            Debug.Log(serializerInUse);
+            
+            if (serializerInUse == RequestSerializerType.JsonUtility)
             {
-                var variableName =
-                    (CodeGenTemplateBuilder.BaseSlug + CodeGenTemplateBuilder.GenericArgumentSlug + (i + 1)).Replace(
-                        "_1", "");
-                builder.SetVariable(variableName, "string");
+                builder.SetVariable("SERIALIZER", "Json");
+                SetGenericArgsToString(builder);
             }
+            if (serializerInUse == RequestSerializerType.MirrorBuiltIn)
+            {
+                builder.SetVariable("SERIALIZER", "MirrorWeaver");
+            }
+            SetResponseVariable(builder, type);
         }
     }
 }
